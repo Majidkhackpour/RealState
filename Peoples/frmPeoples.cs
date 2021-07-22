f40d0338aecd9c225103dfa794084316d1d67dbe
arad_enj <arad_enj@yahoo.com>
2021-07-22 23:59:11 +0430

    Commit

--- Peoples/frmPeoples.cs
+++ Peoples/frmPeoples.cs
@@ -398,11 +398,6 @@ namespace Peoples
             if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
                 e.Handled = true;
         }
-        private void txtName_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (!char.IsLetter(e.KeyChar) && e.KeyChar != (char)Keys.Back && e.KeyChar != (char)Keys.Space)
-                e.Handled = true;
-        }
         private void txtNationalCode_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
