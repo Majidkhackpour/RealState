d40e330f4e3018edc16e25bab1682860c8133083
arad_enj <arad_enj@yahoo.com>
2021-12-02 19:26:53 +0330

    Commit

---
+++ Building/UserControls/Contract/Sell/UcContractSell_5.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace Building.UserControls.Contract.Sell
+{
+    partial class UcContractSell_5
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UcContractSell_5));
+            this.grpPanel = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblContractDate = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAmountOfRent = new WindowsSerivces.CurrencyTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.grpPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpPanel
+            // 
+            this.grpPanel.BackColor = System.Drawing.Color.Transparent;
+            this.grpPanel.CanvasColor = System.Drawing.SystemColors.Control;
+            this.grpPanel.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.grpPanel.Controls.Add(this.txtAmountOfRent);
+            this.grpPanel.Controls.Add(this.label6);
+            this.grpPanel.Controls.Add(this.label1);
+            this.grpPanel.Controls.Add(this.lblContractDate);
+            this.grpPanel.Controls.Add(this.label2);
+            this.grpPanel.Controls.Add(this.label3);
+            this.grpPanel.Controls.Add(this.label4);
+            this.grpPanel.Controls.Add(this.label7);
+            this.grpPanel.DisabledBackColor = System.Drawing.Color.Empty;
+            this.grpPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpPanel.Location = new System.Drawing.Point(0, 0);
+            this.grpPanel.Name = "grpPanel";
+            this.grpPanel.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.grpPanel.Size = new System.Drawing.Size(848, 190);
+            // 
+            // 
+            // 
+            this.grpPanel.Style.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(227)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
+            this.grpPanel.Style.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(175)))), ((int)(((byte)(210)))), ((int)(((byte)(255)))));
+            this.grpPanel.Style.BackColorGradientAngle = 90;
+            this.grpPanel.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.grpPanel.Style.BorderBottomWidth = 2;
+            this.grpPanel.Style.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(195)))), ((int)(((byte)(198)))));
+            this.grpPanel.Style.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(195)))), ((int)(((byte)(198)))));
+            this.grpPanel.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.grpPanel.Style.BorderLeftWidth = 2;
+            this.grpPanel.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.grpPanel.Style.BorderRightWidth = 2;
+            this.grpPanel.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.grpPanel.Style.BorderTopWidth = 2;
+            this.grpPanel.Style.CornerDiameter = 4;
+            this.grpPanel.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded;
+            this.grpPanel.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.grpPanel.Style.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.grpPanel.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near;
+            // 
+            // 
+            // 
+            this.grpPanel.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.grpPanel.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.grpPanel.TabIndex = 3;
+            this.grpPanel.Text = "ماده 5 - شرایط تسلیم مورد معامله";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(-70, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(558, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "با تمامی توابع و منضمات آن تسلیم خریدار نماید و هرگونه موانع در بهره برداری کامل " +
+    "از مورد معامله را برطرف نماید";
+            // 
+            // lblContractDate
+            // 
+            this.lblContractDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblContractDate.AutoSize = true;
+            this.lblContractDate.BackColor = System.Drawing.Color.Transparent;
+            this.lblContractDate.Location = new System.Drawing.Point(494, 18);
+            this.lblContractDate.Name = "lblContractDate";
+            this.lblContractDate.Size = new System.Drawing.Size(85, 20);
+            this.lblContractDate.TabIndex = 11;
+            this.lblContractDate.Text = "1400/12/29";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(533, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(298, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "کلیه هزینه های ناشی از تسلیم بر عهده فروشنده خواهد بود.";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(3, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(830, 67);
+            this.label4.TabIndex = 11;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(585, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(248, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "5-1 فروشنده موظف است مورد معامله را در تاریخ";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(718, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "میزان خسارت برابر با";
+            // 
+            // txtAmountOfRent
+            // 
+            this.txtAmountOfRent.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.txtAmountOfRent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAmountOfRent.BackColor = System.Drawing.Color.White;
+            this.txtAmountOfRent.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.txtAmountOfRent.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txtAmountOfRent.Font = new System.Drawing.Font("B Titr", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtAmountOfRent.Location = new System.Drawing.Point(427, 127);
+            this.txtAmountOfRent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtAmountOfRent.Name = "txtAmountOfRent";
+            this.txtAmountOfRent.Size = new System.Drawing.Size(285, 31);
+            this.txtAmountOfRent.TabIndex = 20;
+            this.txtAmountOfRent.TextDecimal = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(332, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 20);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "ریال خواهد بود.";
+            // 
+            // UcContractSell_5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.grpPanel);
+            this.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "UcContractSell_5";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Size = new System.Drawing.Size(848, 190);
+            this.grpPanel.ResumeLayout(false);
+            this.grpPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.Controls.GroupPanel grpPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblContractDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private WindowsSerivces.CurrencyTextBox txtAmountOfRent;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ Building/UserControls/Contract/Sell/UcContractSell_5.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+namespace Building.UserControls.Contract.Sell
+{
+    public partial class UcContractSell_5 : UserControl
+    {
+        public string DischargeDateSh { set => lblContractDate.Text = value; }
+        public decimal AmountOfRent { get => txtAmountOfRent.TextDecimal; set => txtAmountOfRent.TextDecimal = value; }
+        public UcContractSell_5() => InitializeComponent();
+    }
+}
