74a46da84e7855fc4a906147bf89dd3b352798df
arad_enj <arad_enj@yahoo.com>
2022-02-17 20:22:33 +0330

    Commit

---
+++ Building/UserControls/Report/UcReport.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Building.UserControls.Report
+{
+    partial class UcReport
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucShowSchema1 = new Building.UserControls.Report.MajorObject.UcShowSchema();
+            this.ucShowCustomerBirthday1 = new Building.UserControls.Report.MajorObject.UcShowCustomerBirthday();
+            this.ucShowDischargeList1 = new Building.UserControls.Report.MajorObject.UcShowDischargeList();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.ucShowMatcheItems1 = new Building.UserControls.Report.MajorObject.UcShowMatcheItems();
+            this.ucShowHighPriority1 = new Building.UserControls.Report.MajorObject.UcShowHighPriority();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ucShowSchema1
+            // 
+            this.ucShowSchema1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucShowSchema1.BackColor = System.Drawing.Color.Transparent;
+            this.ucShowSchema1.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.ucShowSchema1.Location = new System.Drawing.Point(688, 4);
+            this.ucShowSchema1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ucShowSchema1.Name = "ucShowSchema1";
+            this.ucShowSchema1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ucShowSchema1.Size = new System.Drawing.Size(228, 222);
+            this.ucShowSchema1.TabIndex = 0;
+            // 
+            // ucShowCustomerBirthday1
+            // 
+            this.ucShowCustomerBirthday1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucShowCustomerBirthday1.BackColor = System.Drawing.Color.Transparent;
+            this.ucShowCustomerBirthday1.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.ucShowCustomerBirthday1.Location = new System.Drawing.Point(427, 4);
+            this.ucShowCustomerBirthday1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ucShowCustomerBirthday1.Name = "ucShowCustomerBirthday1";
+            this.ucShowCustomerBirthday1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ucShowCustomerBirthday1.Size = new System.Drawing.Size(253, 222);
+            this.ucShowCustomerBirthday1.TabIndex = 1;
+            // 
+            // ucShowDischargeList1
+            // 
+            this.ucShowDischargeList1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucShowDischargeList1.BackColor = System.Drawing.Color.Transparent;
+            this.ucShowDischargeList1.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.ucShowDischargeList1.Location = new System.Drawing.Point(4, 4);
+            this.ucShowDischargeList1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ucShowDischargeList1.Name = "ucShowDischargeList1";
+            this.ucShowDischargeList1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ucShowDischargeList1.Size = new System.Drawing.Size(415, 222);
+            this.ucShowDischargeList1.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(4, 232);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.ucShowMatcheItems1);
+            this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.splitContainer1.Size = new System.Drawing.Size(912, 232);
+            this.splitContainer1.SplitterDistance = 380;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // ucShowMatcheItems1
+            // 
+            this.ucShowMatcheItems1.BackColor = System.Drawing.Color.Transparent;
+            this.ucShowMatcheItems1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucShowMatcheItems1.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.ucShowMatcheItems1.Location = new System.Drawing.Point(0, 0);
+            this.ucShowMatcheItems1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ucShowMatcheItems1.Name = "ucShowMatcheItems1";
+            this.ucShowMatcheItems1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ucShowMatcheItems1.Size = new System.Drawing.Size(528, 232);
+            this.ucShowMatcheItems1.TabIndex = 0;
+            // 
+            // ucShowHighPriority1
+            // 
+            this.ucShowHighPriority1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucShowHighPriority1.BackColor = System.Drawing.Color.Transparent;
+            this.ucShowHighPriority1.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.ucShowHighPriority1.Location = new System.Drawing.Point(4, 470);
+            this.ucShowHighPriority1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ucShowHighPriority1.Name = "ucShowHighPriority1";
+            this.ucShowHighPriority1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ucShowHighPriority1.Size = new System.Drawing.Size(912, 142);
+            this.ucShowHighPriority1.TabIndex = 4;
+            // 
+            // UcReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.ucShowHighPriority1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.ucShowDischargeList1);
+            this.Controls.Add(this.ucShowCustomerBirthday1);
+            this.Controls.Add(this.ucShowSchema1);
+            this.Font = new System.Drawing.Font("B Yekan", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "UcReport";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Size = new System.Drawing.Size(920, 616);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MajorObject.UcShowSchema ucShowSchema1;
+        private MajorObject.UcShowCustomerBirthday ucShowCustomerBirthday1;
+        private MajorObject.UcShowDischargeList ucShowDischargeList1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private MajorObject.UcShowMatcheItems ucShowMatcheItems1;
+        private MajorObject.UcShowHighPriority ucShowHighPriority1;
+    }
+}
---
+++ Building/UserControls/Report/UcReport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EntityCache.Bussines;
+using Services;
+
+namespace Building.UserControls.Report
+{
+    public partial class UcReport : UserControl
+    {
+        public UcReport() => InitializeComponent();
+        public async Task InitAsync()
+        {
+            try
+            {
+                var list = new List<Task>
+                {
+                    Task.Run(ucShowCustomerBirthday1.InitAsync),
+                    Task.Run(ucShowDischargeList1.InitAsync),
+                    Task.Run(ucShowHighPriority1.InitAsync),
+                    Task.Run(ucShowMatcheItems1.InitAsync),
+                };
+                await Task.WhenAll(list);
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+        }
+    }
+}
